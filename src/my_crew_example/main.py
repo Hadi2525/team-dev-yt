7336a0fc911bdfd5c6e229aac8032bcdff8d5844
full-stack-ai <fullstackinfo.ai@gmail.com>
2025-01-12 16:50:53 -0700

    first commit - run crewai install + agents updates

---
+++ src/my_crew_example/main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from my_crew_example.crew import MyCrewExample
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    MyCrewExample().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        MyCrewExample().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        MyCrewExample().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        MyCrewExample().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "train":
+        train()
+    elif sys.argv[1] == "replay":
+        replay()
+    elif sys.argv[1] == "test":
+        test()
+    else:
+        run()
